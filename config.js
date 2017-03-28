c4927a2ac50ab9830e66f4b25adc399647a0dc9b
Joy <joylam415@gmail.com>
2017-03-27 20:58:32 -0400

    Remove own API keys

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   twitter: {
-    consumer_key: 'XcjIgnKnBdnHHjxs0cv6rcgoK',
-    consumer_secret: '9PmtVE0V6PnJ0R9ihL1xpr9ZmtLq0DbEY25xGcoGG7UgleZF0i',
-    access_token: '806654771058851845-tHHXKScYXjKkORiRpaKCmEzRL39wQmp',
-    access_token_secret: 'KLFQ5qIrdKcAYavbxUTsscJdoY3DY9MqOyxsUV36X4kGn',
+    consumer_key: '...',
+    consumer_secret: '...',
+    access_token: '...',
+    access_token_secret: '...',
     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   },
   mongodb: {
